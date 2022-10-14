423d6183f305b7a21d06e6bb90e24e58e63417e1
Vitor Rocha <deus.1928@gmail.com>
2022-10-14 14:00:53 -0400

    add soluction 14th oct 2022

---
+++ Leetcode daily/14th_otc.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode* slow = head, *fast = head;
+        ListNode *freshNode = new ListNode(-1,head);
+        ListNode* prev = freshNode;
+        while(fast and fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = slow->next;
+        return freshNode->next;
+    }
+};
