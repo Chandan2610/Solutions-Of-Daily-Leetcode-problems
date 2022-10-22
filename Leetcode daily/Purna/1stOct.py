4e796c43b6f14bdfa8c32677fb9d162ae0d25a6b
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2022-10-22 19:04:25 +0530

    Create 1stOct.py

---
+++ Leetcode daily/Purna/1stOct.py
@@ -0,0 +1,11 @@
+ int numDecodings(string s) {
+        return s.empty() ? 0: numDecodings(0,s);    
+    }
+    int numDecodings(int p, string& s) {
+        int n = s.size();
+        if(p == n) return 1;
+        if(s[p] == '0') return 0; // sub string starting with 0 is not a valid encoding
+        int res = numDecodings(p+1,s);
+        if( p < n-1 && (s[p]=='1'|| (s[p]=='2'&& s[p+1]<'7'))) res += numDecodings(p+2,s);
+        return res;
+    }
