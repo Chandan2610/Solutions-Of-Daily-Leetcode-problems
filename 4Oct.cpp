6c8cdf8e760244bb2734125a853c9bb6c8f8cf7f
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2022-10-22 19:28:23 +0530

    Create 4Oct.cpp

---
+++ 4Oct.cpp
@@ -0,0 +1,5 @@
+bool hasPathSum(TreeNode *root, int sum) {
+        if (root == NULL) return false;
+        if (root->val == sum && root->left ==  NULL && root->right == NULL) return true;
+        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+    }
